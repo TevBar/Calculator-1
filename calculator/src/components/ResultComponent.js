4aacc33388806f22a6db09d566854db1275a2b5b
Tevin Barrios <BarriosTevin@gmail.com>
2024-01-06 20:13:33 -0800

    first commit

---
+++ calculator/src/components/ResultComponent.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react'
+
+class ResultComponent extends Component { 
+    render() {
+        let {result} = this.props;
+        return (
+            <div className = "result">
+                <p>{result}</p>
+            </div>
+        )
+    };
+
+    }
+
+
+export default ResultComponent;
+
+
+
+
+
+
+// import React, {Component} from 'react';
+
+// class ResultComponent extends Component {
+
+
+//     render() {
+//         let {result} = this.props;
+//         return (
+//             <div className="result">
+//                 <p>{result}</p>
+//             </div>
+//     )
+//         ;
+//     }
+// }
+
+
+// export default ResultComponent;
---
+++ notes.js
@@ -0,0 +1,45 @@
+// control- e is a shortcut used to move the cursor to the end of the line
+
+// the output class on line 12 represents the calculator screen
+
+// prev operand and current operand represents the previous operation listed and the current operation listed in the calculator
+
+// the span-two class represents the two columns occupied on the calculator 
+
+
+
+//* */ grid-column: 1 /-1;
+// setting the output to span cross the entire width of the calculator,
+// we do this using the grid-column attribute 
+
+// set output element to wrap when they get too long. 
+// word-wrap is used when we want to choose where the words should break if they are too long
+
+
+// ** reminder on constant variables (const- interchangable variables)
+
+// Constant variables, often referred to simply as "constants," are variables whose values cannot be changed after they are defined. 
+// In many programming languages, including JavaScript, Python, and C++, 
+// constants provide a way to store and refer to values that should remain fixed throughout 
+// the execution of a program. 
+
+
+// Code to handle the event when the dot key is pressed
+        // This code block will execute if event.key is a dot.
+        // if(event.key == '.')
+
+
+        // this.operation is set to undefined because when we clear the calculator there is no operation button selected
+        // so naturally undefined = nothing chosen.
+
+
+//  locale string is basically the language selected 
+
+
+
+
+
+
+
+
+        
